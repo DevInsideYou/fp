1d6e74c4e812bc2a134c7aaf1d61cab791126c78
Vladyslav Pekker <agilesteel@gmail.com>
2022-07-16 21:42:48 -0300

    mutable state must be delayed

--- src/main/scala/dev/insideyou/fp/Mutability.scala
+++ src/main/scala/dev/insideyou/fp/Mutability.scala
@@ -8,6 +8,8 @@ import fplibrary.*
 object MutableProgram:
   final class MutableBankAccount(initialBalance: Int):
     private var currentBalance: Int =
+      println("sending rockets to space")
+
       initialBalance
 
     def balance: Int =
@@ -22,18 +24,23 @@ object MutableProgram:
     override def toString: String =
       s"MutableBankAccount($balance)"
 
+  val tooEarly = MutableBankAccount(initialBalance = 0)
+
   val value =
     import FPConsole.*
 
     for
       _ <- println("─" * 50)
-      b <- IO.delay(MutableBankAccount(initialBalance = 0))
-      _ <- IO.delay(b.deposit(amount = 20))
-      _ <- IO.delay(b.withdraw(amount = 5))
+      b = tooEarly
+      _ = b.deposit(amount = 20)
+      _ = b.withdraw(amount = 5)
       _ <- println(b)
       _ <- println("─" * 50)
     yield ()
 
-object Mutability extends FPApp:
-  override lazy val run =
-    MutableProgram.value
+// object Mutability extends FPApp:
+//   override lazy val run =
+//     MutableProgram.value
+
+@main def Mutability =
+  MutableProgram.value
